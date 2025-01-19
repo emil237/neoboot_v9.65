0a19d0bada52a4499d40ef4b52b8824d4cababd7
emil237 <marianatalaat1@gmail.com>
2025-01-19 17:23:25 +0200

    Add files via upload

--- iNB_9.65_mod-elsafty.sh
+++ iNB_9.65_mod-elsafty.sh
@@ -14,19 +14,17 @@ REQUIRED='/usr/lib/enigma2/python/Plugins/Extensions/NeoBoot/files'
 TOOLS='/usr/lib/enigma2/python/Tools'
 PREDION='/usr/lib/periodon'
 ##########################################
-##########################################
 PYTHON_VERSION=$(python -c "import platform; print(platform.python_version())")
 
 ###########################################
 
 # remove old version
-if [ -d "$PLUGINPATH" ]; then
-   rm -rf "$PLUGINPATH"
+if [ -d $PLUGINPATH ]; then
+   rm -rf $PLUGINPATH 
 fi
 
 # Python Version Check #
-PYTHON=$(python --version 2>&1)
-if [[ $PYTHON == "Python 3."* ]]; then
+if python --version 2>&1 | grep -q '^Python 3\.'; then
    echo "You have Python3 image"
    PYTHON='PY3'
 else
@@ -60,20 +58,28 @@ esac
 
 #########################
 install() {
-    if ! grep -qs "Package: $1" "$STATUS"; then
+    if ! grep -qs "Package: $1" $STATUS; then
         $OPKG >/dev/null 2>&1
         echo "   >>>>   Need to install $1   <<<<"
         echo
-        $OPKGINSTAL "$1"
-        sleep 1
-        clear
+        if [ $OSTYPE = "Opensource" ]; then
+            $OPKGINSTAL "$1"
+            sleep 1
+            clear
+        fi
     fi
 }
 
 #########################
-for i in kernel-module-nandsim mtd-utils-jffs2 lzo python-setuptools util-linux-sfdisk packagegroup-base-nfs ofgwrite bzip2 mtd-utils mtd-utils-ubifs; do
-    install "$i"
-done
+if [ $PYTHON = "PY3" ]; then
+    for i in kernel-module-nandsim mtd-utils-jffs2 lzo python-setuptools util-linux-sfdisk packagegroup-base-nfs ofgwrite bzip2 mtd-utils mtd-utils-ubifs; do
+        install $i
+    done
+else
+    for i in kernel-module-nandsim mtd-utils-jffs2 lzo python-setuptools util-linux-sfdisk packagegroup-base-nfs ofgwrite bzip2 mtd-utils mtd-utils-ubifs; do
+        install $i
+    done
+fi
 
 #########################
 clear
@@ -82,21 +88,22 @@ opkg update
 echo "   UPLOADED BY  >>>>   EMIL_NABIL " 
 sleep 4
 echo " SUPPORTED BY  >>>>  linuxsat  " 
-sleep 4
-echo " Moded By Elsafty"
-sleep 4
-echo "***********************************************************************
-#########################
-clear                                                                               
-echo "**********************************************************************************"
+echo " Moded By Elsafty "
+sleep 6
+echo "***********************************************************************"
+echo " download and install plugin "
+cd /tmp
+set -e 
 wget -O /var/volatile/tmp/enigma2-plugin-extensions-neoboot-v9.65-by-linuxsat_mod-elsafty.tar.gz "https://github.com/emil237/download-plugins/raw/main/enigma2-plugin-extensions-neoboot-v9.65-by-linuxsat_mod-elsafty.tar.gz"
 wait
-tar -xzf /var/volatile/tmp/enigma2-plugin-extensions-neoboot-v9.65-by-linuxsat_mod-elsafty.tar.gz -C /
+tar -xzf enigma2-plugin-extensions-neoboot-v9.65-by-linuxsat_mod-elsafty.tar.gz -C /
+cd ..
+set +e
 rm -f /var/volatile/tmp/enigma2-plugin-extensions-neoboot-v9.65-by-linuxsat_mod-elsafty.tar.gz
 
 #########################
 clear
-cd "$PLUGINPATH"
+cd $PLUGINPATH
 chmod 755 ./bin/*
 chmod 755 ./ex_init.py
 chmod 755 ./files/*.sh
@@ -110,16 +117,14 @@ echo $MY_EM
 echo "**                       NeoBoot  : $VERSION                          *"
 echo "**                                                                    *"
 echo "***********************************************************************"
-echo ". >>>>         RESTARTING     <<<<"
+echo " >>>>         RESTARING     <<<<"
 echo ""
-if [ "$OSTYPE" = 'DreamOS' ]; then
+if [ $OSTYPE = 'DreamOS' ]; then
     systemctl restart enigma2
 else
     init 6
 fi
 exit 0
-
-
 
 
 
